114b787a1a88c8cd2d8e02367378cdec74af86ea
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-08-12 23:51:18 +0000

    Do task 7, console

--- console.py
+++ console.py
@@ -1,10 +1,115 @@
 #!/usr/bin/python3
 '''The Console a CommandLine Interpreter'''
 import cmd
+# from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """ A class that inherits a cmd class from the module."""
+
+    # def do_create(self, args):
+    #     ''' Create a new instance of BaseModel,
+    #         save it to JSON and prints the id
+    #         Usage: create <class name>
+    #     '''
+    #     args = args.split()
+    #     if len(args) == 0:
+    #         print("** class name missing **")
+    #     elif args[0] not in HBNBCommand.__classes:
+    #         print("** class doesn't exist **")
+    #     else:
+    #         new_creation = eval(args[0] + '()')
+    #         storage.save()
+    #         print(new_creation.id)
+
+#     def do_show(self, args):
+#         '''Prints the string representation of a specific instance
+#            Usage: show <class name> <id>
+#         '''
+#         strings = args.split()
+#         if len(strings) == 0:
+#             print("** class name missing **")
+#         elif strings[0] not in HBNBCommand.__classes:
+#             print("** class doesn't exist **")
+#         elif len(strings) == 1:
+#             print("** instance id missing **")
+#         else:
+#             obj = models.storage.all()
+#             key_value = strings[0] + '.' + strings[1]
+#             if key_value in obj:
+#                 print(obj[key_value])
+#             else:
+#                 print("** no instance found **")
+
+#     def do_destroy(self, args):
+#         '''Delete an instance
+#            Usage: destroy <class name> <id>
+#         '''
+#         args = args.split()
+#         objects = models.storage.all()
+
+#         if len(args) == 0:
+#             print('** class name missing **')
+#         elif args[0] not in HBNBCommand.__classes:
+#             print("** class doesn't exist **")
+#         elif len(args) == 1:
+#             print('** instance id missing **')
+#         else:
+#             key_find = args[0] + '.' + args[1]
+#             if key_find in objects.keys():
+#                 objects.pop(key_find, None)
+#                 models.storage.save()
+#             else:
+#                 print('** no instance found **')
+
+#     def do_all(self, args):
+#         '''Print all string representation of all instances
+#            Usage: all <class name>
+#         '''
+#         args = args.split()
+#         objects = models.storage.all()
+#         new_list = []
+
+#         if len(args) == 0:
+#             for obj in objects.value():
+#                 new_list.append(obj.__str__())
+#             print(new_list)
+#         elif args[0] not in HBNBCommand.__classes:
+#             print("** class doesn't exist **")
+#         else:
+#             for obj in objects.values():
+#                 if obj.__class__.__name__ == args[0]:
+#                     new_list.append(obj.__str__())
+#             print(new_list)
+
+#     def do_update(args, self):
+#         ''' Update an instance
+#             Usage update:
+#             <class name> <id> <attribute name> "<attribute value>"
+#         '''
+#         objects = models.storage.all()
+#         args = args.split(" ")
+
+#         if len(args) == 0:
+#             print("** class name missing **")
+#         elif args[0] not in HBNBCommand.__classes:
+#             print("** class doesn't exist **")
+#         elif len(args) == 1:
+#             print("** instance id missing **")
+#         elif len(args) == 2:
+#             print("** attribute name missing **")
+#         elif len(args) == 3:
+#             print("** value missing **")
+#         else:
+#             key_find = args[0] + '.' + args[1]
+#             obj = objects.get(key_find, None)
+
+#             if not obj:
+#                 print("** no instance found **")
+#                 return
+#             setattr(obj, args[2], args[3].lstrip('"').rstrip('"'))
+#             models.storage.save()
+
 
     prompt = '(hbnb) '
 
