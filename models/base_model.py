fa5702d0a69ad1caa514cff4d4c1211882651879
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-08-12 17:06:30 +0000

    Do task 3, BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ This module is composed of BaseModel class."""
-import datetime
-import uuid
+from datetime import datetime as dt
+from uuid import uuid4
 
 
 class BaseModel:
     """ Defines all common attributes/methods for other classes."""
 
-    def __init__(self, id=str(uuid.uuid4()),
-                 created_at=datetime.datetime.now(),
-                 updated_at=datetime.datetime.now()):
+    def __init__(self, id=str(uuid4()),
+                 created_at=dt.now(),
+                 updated_at=dt.now()):
         """ Initializes a BaseModel object.
 
             Params:
@@ -30,7 +30,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at
             with the current datetime."""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = dt.now()
 
     def to_dict(self):
         """ returns a dictionary containing all
