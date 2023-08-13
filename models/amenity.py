3848d9c64c1fa4d5770fe286e447911f0307b1d5
AchrafEs <achraf.es2110@gmail.com>
2023-08-13 12:35:13 +0100

    updated the amenity

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Inherent of BaseModel'''
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    '''amenity class'''
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
