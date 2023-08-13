295284b6ed5f13fed10b8027118fff6e28088489
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-08-13 10:47:47 +0000

    Fix task 5, storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """ This module is composed of FileStorage class."""
 import json
+import os
 
 
 class FileStorage:
     """ A class that serializes instances to a
-        JSON file and deserializes JSON file to instances."""
+        JSON file and deserializes JSON file to instances.
 
+        Attributes:
+            __file_path (string): stores the path to JSON file.
+            __objects (dict): stores class objects."""
     __file_path = "file.json"
     __objects = {}
-
-    def __init__(self):
-        """ Initializes FileStorage instance.
-
-            Private attributes:
-                __file_path (string): stores the path to JSON file.
-                __objects (dict): stores class objects."""
 
     def all(self):
         """ Returns the __objects dictionary."""
@@ -23,7 +20,7 @@ class FileStorage:
 
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj.to_dict()
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj.to_dict()
 
     def save(self):
         """ Serializes __objects to the JSON file (path: __file_path)"""
@@ -32,8 +29,7 @@ class FileStorage:
 
     def reload(self):
         """ Deserializes the JSON file to __objects."""
-        try:
+
+        if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as f:
                 FileStorage.__objects = json.load(f)
-        except FileNotFoundError:
-            pass
