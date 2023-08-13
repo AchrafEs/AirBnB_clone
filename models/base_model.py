dae51e0b31a87a6d83c13113062fc0de07ddeecd
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-08-13 01:16:29 +0000

    Fix task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,7 @@ from uuid import uuid4
 class BaseModel:
     """ Defines all common attributes/methods for other classes."""
 
-    def __init__(self,
-                 id=str(uuid4()),
-                 created_at=dt.now(),
-                 updated_at=dt.now()):
+    def __init__(self):
         """ Initializes a BaseModel object.
 
             Params:
@@ -20,13 +17,13 @@ class BaseModel:
                                 when an instance is created.
                 updated_at (datetime): assigned and updated
                                 when an instance is created or changed."""
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid4())
+        self.created_at = dt.now()
+        self.updated_at = dt.now()
 
     def __str__(self):
         """ prints: [<class name>] (<self.id>) <self.__dict__>"""
-        return (f"['BaseModel'] ({self.id}) {self.__dict__}")
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """ updates the public instance attribute updated_at
@@ -36,9 +33,9 @@ class BaseModel:
     def to_dict(self):
         """ returns a dictionary containing all
             keys/values of __dict__ of the instance. """
-        self.__dict__["__class__"] = "BaseModel"
-        self.__dict__["created_at"] = self.__dict__["created_at"].\
-            strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.__dict__["updated_at"] = self.__dict__["updated_at"].\
-            strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.__dict__['__class__'] = self.__class__.__name__
+        self.__dict__['created_at'] = self.__dict__['created_at'].\
+            isoformat()
+        self.__dict__['updated_at'] = self.__dict__['updated_at'].\
+            isoformat()
         return self.__dict__
